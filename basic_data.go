04648c2df09acbd252ecd8a2ea2b0417489f8f85
niannian.chen <niannian.chen@denglinai.com>
2023-11-04 23:00:58 +0800

    feat: add sw

--- basic_data.go
+++ basic_data.go
@@ -302,3 +302,14 @@ func (api *TuShare) HmDetail(params map[string]string, fields []string) (*APIRes
 	}
 	return api.postData(body)
 }
+
+// SwDaily 万申指数日线行情
+func (api *TuShare) SwDaily(params map[string]string, fields []string) (*APIResponse, error) {
+	body := map[string]interface{}{
+		"api_name": "sw_daily",
+		"token":    api.token,
+		"params":   params,
+		"fields":   fields,
+	}
+	return api.postData(body)
+}
